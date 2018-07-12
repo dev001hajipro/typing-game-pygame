f402319697734dd4463bbb30769b1f73c39ce426
dev001hajipro <dev001hajipro@gmail.com>
2018-07-12 18:05:45 +0900

    空行削除

--- main_slim.py
+++ main_slim.py
@@ -12,7 +12,6 @@ def run():
     words = []
     with open('words.txt') as csvfile:
         words = list(map(lambda s: s.rstrip(), list(csvfile.readlines())))
-
     pygame.mixer.pre_init(44100, -16, 1, 512)
     pygame.mixer.init()
     pygame.init()
@@ -20,28 +19,20 @@ def run():
     se_keytype = pygame.mixer.Sound('.\\se\\keytype2.wav')
     fonts = [pygame.font.SysFont(None, 24), pygame.font.SysFont(None, 128)]
     screen = pygame.display.set_mode((720, 480))
-
     playing, start_ticks, timer, score, word = (
         False, pygame.time.get_ticks(), 10, 0, 'press space key.')
-
     while True:
         screen.fill((240, 240, 240))
-
         if playing:
             timer = 10 - int((pygame.time.get_ticks() - start_ticks) / 1000)
             if timer <= 0:
-                playing = False
-                word = 'press space key.'
-
-        text_time_score = fonts[0].render(
-            f"time:{timer} score:{score}", True, (0, 0, 0))
+                playing, word = (False, 'press space key.')
+        screen.blit(fonts[0].render(
+            f"time:{timer} score:{score}", True, (0, 0, 0)), (10, 10))
         text_word = fonts[1].render(f"{word}", True, (0, 0, 0))
-        screen.blit(text_time_score, (10, 10))
         screen.blit(text_word, (screen.get_size()[0]/2 - text_word.get_size()[0] /
                                 2, screen.get_size()[1]/2 - text_word.get_size()[1]/2))
-
         pygame.display.update()
-
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 change_word_flag = False
@@ -56,8 +47,6 @@ def run():
                 if change_word_flag:
                     word = words[random.randint(0, len(words) - 1)]
             elif event.type == pygame.QUIT:
-                pygame.mixer.quit()
-                pygame.quit()
                 sys.exit()
 
 
